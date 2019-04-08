492b40abc80b1424cbf3a386039cec4767c36f63
Civoremo <omerovic_n@hotmail.com>
2019-04-08 15:18:06 -0400

    graph_demo.py tests

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,14 @@ Simple graph implementation
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        # pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = set()
+
+    def add_directed_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError('The vertex does not exist.')
